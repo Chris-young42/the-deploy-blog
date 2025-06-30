11afbb4fa09f7a5cc6aff9cce112dc60f8aeb58e
Chris-young42 <3101940893@qq.com>
2025-06-30 19:55:47 +0800

    22

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google'
+import { Outfit } from 'next/font/google'
 import "./globals.css";
-const inter = Inter({ subsets: ['latin'] })
 
+const outfit = Outfit({ subsets: ['latin'], weight: ['400', '500', '600', '700'] })
 export const metadata = {
   title: "my-blog App",
   description: "my blog",
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
+      <body className={`${outfit.className}`}>
         {children}
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className=''>page</div>
+    <>
+      <Header />
+    </>
   )
 }
-
 export default page
