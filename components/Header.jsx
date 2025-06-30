8c2ae2547bebd870804328ca5bd7ae31bfea63bf
Chris-young42 <3101940893@qq.com>
2025-06-30 20:26:02 +0800

    55

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,7 +8,11 @@ const Header = () => {
             <div className='flex justify-between items-center'>
                 <Image src={assets.logo} width={180} alt='my-blog' className='w-[130px] sm:w-auto' />
                 <button
-                    className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started<Image src={assets.arrow}/></button>
+                    className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started<Image src={assets.arrow} /></button>
+            </div>
+            <div className='text-center my-8'>
+                <h1 className='text-3xl sm:text-5xl font-medium'>最近的博客</h1>
+                <p></p>
             </div>
         </div>
     )
