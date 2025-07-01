f2aff38fd85729d420c862ac3e753644ca80a861
Chris-young42 <3101940893@qq.com>
2025-07-02 04:45:36 +0800

    66666

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@
 import Header from '@/components/Header'
 import React from 'react'
 import BlogList from '@/components/BlogList'
+import Footer from '@/components/Footer'
 
 const page = () => {
   return (
     <>
       <Header />
       <BlogList/>
+      <Footer/>
     </>
   )
 }
--- components/BlogItem.jsx
+++ components/BlogItem.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
 import { assets, blog_data } from '@/Assets/assets'
+import Link from 'next/link'
 
-const BlogItem = ({title,description,category,image}) => {
+const BlogItem = ({ title, description, category, image, id }) => {
   return (
     <div className='max-w-[330px] sm:max-w-[300px] bg-white border-black hover:shadow-[-7px_7px_0px_#000000]'>
-      <Image src={image} alt='my-blog' width={400} height={400} className='border-b border-black' />
+      <Link href={`/blogs/${id}`}>
+        <Image src={image} alt='my-blog' width={400} height={400} className='border-b border-black' />
+      </Link>
       <p className='ml-5 px-1 mt-5 inline-block bg-black text-white text-sm'>{category}</p>
       <div className="p-5">
         <h5 className='mb-2 text-lg font-medium tracking-tighter text-gray-900'>{title}</h5>
         <p className='mb-3 text-sm tracking-tighter text-gray-700'>{description}</p>
-        <div className='inline-flex items-center py-2 font-semibold text-center'>
+        <Link href={`/blogs/${id}`} className='inline-flex items-center py-2 font-semibold text-center'>
           阅读更多<Image src={assets.arrow} alt='my-blog' className='ml-2' width={12} />
-        </div>
+        </Link>
       </div>
     </div>
   )
